b73e55c311cd3f52d9b9b3086a8e0964cf9d7a62
Joseph Hunkeler <jhunkeler@gmail.com>
2018-12-19 14:53:19 -0500

    Replace ah_bootstrap

--- specutils/bld.bat
+++ specutils/bld.bat
@@ -1,1 +1,2 @@
+curl -LO https://raw.githubusercontent.com/astropy/package-template/00a38b15c46d1ce049a42961251b4ab2a7178e40/ah_bootstrap.py
 %PYTHON% setup.py install --single-version-externally-managed --record=root.txt
